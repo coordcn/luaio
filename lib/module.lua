cefbcf4481b0135f665cb613be35167ac869da2c
coordcn <coordcn@163.com>
2016-04-29 09:55:24 +0800

    improve: function Module:load_from_path

--- lib/module.lua
+++ lib/module.lua
@@ -8,10 +8,8 @@ local strlib = require('strlib')
 local SPACE8 = strlib.SPACE8
 local LF = strlib.LF
 local grequire = _G.require
-local strbyte = string.byte
-local strmatch = string.match
 
-local modules_dir = path.dirname(process.execpath) .. '/modules/'
+local modules_dir = process.cwd .. '/modules/'
 local lua_ext = '.lua'
 local bin_ext = system.type == 'Windows' and '.dll' or '.so'
 local modules_cache = {}
@@ -39,24 +37,27 @@ local function get_real_path(file)
   return nil
 end
 
-local function err_nofile(name)
-  local err = 'require(' .. name .. ') error: no such files' .. LF ..
-              SPACE8 .. name .. LF ..
-              SPACE8 .. name .. lua_ext .. LF ..
-              SPACE8 .. name .. bin_ext .. LF
-  return err
+local function err_nofile(name, filename)
+  filename = filename or name
+  local errstr = 'require(\'' .. name .. '\') error: no such files' .. LF ..
+                 SPACE8 .. filename .. LF ..
+                 SPACE8 .. filename .. lua_ext .. LF ..
+                 SPACE8 .. filename .. bin_ext .. LF
+  return errstr
 end
 
-function Module:init(file_path, deps)
+function Module:init(file_path, package)
+  package = package or {}
   self.path = file_path
   self.dir = path.dirname(file_path)
-  self.deps = deps or {}
+  self.package = package
+  local deps = package.deps or {}
+  self.deps = deps
   return 0
 end
 
-function Module:load_from_path(real_path, deps)
-  local ext = strmatch(real_path, '.(%.[^%./\\]*)$')
-  local fn, err
+function Module:load_from_path(real_name, real_path, deps)
+  local ext = path.extname(real_path)
   if ext == lua_ext then
     local module = Module:new(real_path, deps)
     local env = {
@@ -66,94 +67,66 @@ function Module:load_from_path(real_path, deps)
       end
     }
 
-    fn, err = loadfile(real_path, 'bt', setmetatable(env, { __index = _G }))
+    local fn, err = loadfile(real_path, 'bt', setmetatable(env, { __index = _G }))
     if not fn then
-      error(err .. '\n' .. debug.traceback())
+      error('require(\'' .. real_name .. '\') error: ' .. LF .. err .. LF .. debug.traceback())
     end
     return fn()
   elseif ext == bin_ext then
-    fn, err = package.loadlib(real_path, fn_name)
+    local fn_name = 'luaopen_' .. real_name
+    local fn, err = package.loadlib(real_path, fn_name)
     if not fn then
-      error(real_path .. "#" .. fn_name .. ": " .. err)
+      error('require(\'' .. real_name .. '\') error: ' .. LF .. err .. LF .. debug.traceback())
     end
     return fn()
   else
-    error()
+    local errstr = 'require(\'' .. real_name .. '\') error: extname must be ' .. lua_ext ..
+                   ' or ' .. bin_ext .. LF .. 
+                   debug.traceback()
+    error(errstr)
   end
 end
 
+-- TODO 模块加载
+-- version = major.minor.patch(releaseinfo)
+-- 优先在版本库中匹配版本字符串完全相同的版本，版本不完全匹配，则给出警告。
+-- 如无完全相同的版本匹配，忽略releaseinfo，比较major.minor.patch。
+-- major.minor.patch都有的，三者完全相同的版本匹配
+-- major.minor，匹配major.minor版本下patch最高的版本
+-- major，匹配major版本下minor.patch最高的版本
+-- nil或''，匹配major.minor.patch最高的版本
+-- 本地加载
+-- 远程加载（git）
+-- 本地编译
 function Module:load_from_modules(name, version)
   version = version or self.deps[name]
-  print(version)
-  if version then
-    -- redis@0.1.0
-    local module_name = name .. '@' .. version
-    if module_name and modules_cache[module_name] then
-      return modules_cache[module_name], nil
-    end
-    
-    local module_version_dir = path.join(modules_dir, name, version)
-    local stat, err = fs.stat(module_version_dir)
-    if err < 0 then
-      error('fs.stat(' .. module_version_dir .. '): ' .. ERRNO.parser(err))
-    end
 
-    if stat.type ~= fs.DIR then
-      error('require(' .. name .. ') error: ' .. module_version_dir .. 'is not a directory')
-    end
-
-    local module_version_package = path.join(module_version_dir, 'package.lua')
-  print(module_version_package)
-    local package_fn = assert(loadfile(module_version_package))
-    local package = package_fn()
-    if not package then
-      error('no package')
-    end
-
-    local main = package.main
-  print(main)
-    if not main then
-      error('no package.main')
-    end
-
-    local file = path.join(module_version_dir, main) .. lua_ext
-    local module = Module:new(file, package.deps or self.deps)
-    local env = {
-      module = module,
-      require = function(name, version)
-        if not name or type(name) ~= 'string' then
-          error('require(name) error: name must be string')
-        end
-
-        local ret, errstr = module:require(name, version)
-        print(errstr)
-        if ret then return ret end
-        assert(errstr)
-      end
-    }
-    local fn = assert(loadfile(file, 'bt', setmetatable(env, { __index = _G })))
-    local ret = fn()
-    if ret then return ret end
-  else
-    local module_dir = path.join(modules_dir, name)
-    local stat, err = fs.stat(module_dir)
-    if err < 0 then
-      -- get from net
-      error('fs.stat(' .. module_dir .. '): ' .. ERRNO.parse(err))
-    end
-
-    if stat.type ~= fs.DIR then
-      error('require(' .. name .. ') error: ' .. module_dir .. ' is not a directory')
-    end
-
-    local files
-    files, err = fs.readdir(module_dir) 
-    if err < 0 then
-      error('fs.readdir(' .. module_dir .. ') error: ' .. ERRNO.parser(err))
-    end
-
-  end
 end
+
+local DOT_CODE = strlib.CODE['.']
+local SLASH_CODE = strlib.CODE['/']
+local BACKSLASH_CODE = strlib.CODE['\\']
+
+local function path_is_relative(path)
+  local c1, c2, c3 = path:byte(1, 3)
+  -- './'
+  if c1 == DOT_CODE then
+    if c2 == SLASH_CODE or c2 == BACKSLASH_CODE then
+      return true
+    end
+
+    -- '../'
+    if c2 == DOT_CODE then
+      if c3 == SLASH_CODE or c3 == BACKSLASH_CODE then
+        return true
+      end
+    end
+  end
+
+  return false
+end
+
+local grequire_not_found_reg = '^module .* not found:'
 
 -- @brief require module
 -- @example local ret = require(name[, version])
@@ -163,27 +136,27 @@ end
 --       require('process')
 --    2. modules 'cwd/modules/redis/0.1.0/'    
 --       require('redis'[, '0.1.0'])
+--       modules 'cwd/modules/db/redis/0.1.0/'    
+--       require('db/redis'[, '0.1.0'])
 --    3. relative path module
 --       require('./test')
+--       require('../../test')
 --    4. absolute path module
 --       require('/home/public/lib/test')
+--       require('e:/public/lib/test')
 -- @param version {string} '3.10.8'
 -- @return ret {any}
 function Module:require(name, version)
   if not name or type(name) ~= 'string' then
-    error('require(name) error: name must be string' .. LF .. debug.traceback())
+    error('require(name[, version]) error: name must be string' .. LF .. debug.traceback())
   end
 
-  -- require('./test')
-  local is_relative = strbyte(name, 1) == 46
-  -- require('/home/public/lib/test')
-  local is_absolute = path.isAbsolute(name)
-
   local real_path
-  if is_relative then
-    name = path.join(self.dir, name)
-    real_path = get_real_path(name)
-  elseif is_absolute then
+  local resolved_path
+  if path_is_relative(name) then
+    resolved_path = path.resolve(self.dir, name)
+    real_path = get_real_path(resolved_path)
+  elseif path.isAbsolute(name) then
     real_path = get_real_path(name)
   else
     if package.loaded[name] then
@@ -198,23 +171,25 @@ function Module:require(name, version)
     -- not loaded internal module
     local success, value = pcall(grequire, name)
     if success then return value end
-    -- start with error e = 101
-    if strbyte(value, 1) == 101 then
-      error(value)
+    if not value:match(grequire_not_found_reg) then
+      local errstr = 'require(' .. name .. ') error:' .. LF .. 
+                     SPACE8 .. value .. LF .. 
+                     debug.traceback()
+      error(errstr)
     end
 
     return self:load_from_modules(name, version)
   end
 
   if not real_path then
-    error(err_nofile(name) .. debug.traceback())
+    error(err_nofile(name, resolved_path) .. debug.traceback())
   end
 
   if modules_cache[real_path] then
     return module_cache[real_path]
   end
 
-  local ret = self:load_from_path(real_path, self.deps)
+  local ret = self:load_from_path(name, real_path, self.deps)
   if ret then module_cache[real_path] = ret end
   return ret
 end
